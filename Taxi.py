d57f3e6b45c857c5497664d08ea0d6ea9df68d16
adnan12256 <94077298+adnan12256@users.noreply.github.com>
2022-09-03 01:06:24 -0400

    Add files via upload

---
+++ Taxi.py
@@ -0,0 +1,140 @@
+from random import randint
+
+
+class Pos:
+    xpos = 0
+    ypos = 0
+
+    def __init__(self, x, y):
+        Pos.xpos = Pos.xpos + x
+        Pos.ypos = Pos.ypos + y
+
+
+game_path = ["| | | | | | | | | | |",
+             "| | | | | | | | | | |",
+             "| | | | | | | | | | |",
+             "| | | | | | | | | | |",
+             "| | | | | | | | | | |"]
+
+
+def print_path():
+    for i in game_path:
+        print(i)
+
+
+def add_value(num, cell, pointer):
+    # print(num, cell)
+    temp_string = ""
+    for character in game_path[cell]:
+        temp_string += character
+    index = (num * 2) + 1
+    temp_string = temp_string[:index] + pointer + temp_string[index + 1:]
+    game_path[cell] = temp_string
+
+
+def moving_taxi(target_x, target_y):
+    moving = True
+    while moving:
+        if Pos.xpos == target_x and Pos.ypos == target_y:
+            add_value(Pos.xpos, Pos.ypos, " ")
+            add_value(passenger_posx, passenger_posy, " ")
+            break
+        move = input("\nMake your move (WASD) or press any other key to quit. Position: " + str(Pos.xpos) + "," + str(Pos.ypos) + "\n")
+        if move == "w":
+            add_value(Pos.xpos, Pos.ypos, " ")
+            Pos(0, -1)
+            if Pos.ypos < 0:
+                print("Invalid move!")
+                Pos(0, 1)
+                add_value(Pos.xpos, Pos.ypos, "T")
+                print_path()
+                continue
+            else:
+                add_value(Pos.xpos, Pos.ypos, "T")
+                print_path()
+        elif move == "a":
+            add_value(Pos.xpos, Pos.ypos, " ")
+            Pos(-1, 0)
+            if Pos.xpos < 0:
+                print("Invalid move!")
+                Pos(1, 0)
+                add_value(Pos.xpos, Pos.ypos, "T")
+                print_path()
+                continue
+            else:
+                add_value(Pos.xpos, Pos.ypos, "T")
+                print_path()
+        elif move == "s":
+            add_value(Pos.xpos, Pos.ypos, " ")
+            Pos(0, 1)
+            if Pos.ypos > 4:
+                print("Invalid move!")
+                Pos(0, -1)
+                add_value(Pos.xpos, Pos.ypos, "T")
+                print_path()
+                continue
+            else:
+                add_value(Pos.xpos, Pos.ypos, "T")
+                print_path()
+        elif move == "d":
+            add_value(Pos.xpos, Pos.ypos, " ")
+            Pos(1, 0)
+            if Pos.xpos > 9:
+                print("Invalid move!")
+                Pos(-1, 0)
+                add_value(Pos.xpos, Pos.ypos, "T")
+                print_path()
+                continue
+            else:
+                add_value(Pos.xpos, Pos.ypos, "T")
+                print_path()
+        else:
+            exit()
+            # add_value(Pos.xpos, Pos.ypos, " ")
+            # add_value(passenger_posx, passenger_posy, " ")
+            # moving = False
+
+
+print("WELCOME TO THE TAXI GAME!!")
+game = True
+
+while game:
+    main_menu = input("Press P to play, I for instructions and press anything else to exit the game! ")
+    if main_menu == "p":
+        print("```````````````````````````````````````````````````````````````````````````````````````````````````````")
+        print("Passanger is waiting. Pick up the passanger!")
+        print("```````````````````````````````````````````````````````````````````````````````````````````````````````")
+        passenger_posx, passenger_posy = randint(0, 9), randint(0, 4)
+        add_value(passenger_posx, passenger_posy, "P")
+        # Taxi position is saved to Pos class. It can be modifed by using Pos(x,y). For example, Pos(0, 1) would
+        # move the taxi one unit down
+        Pos.xpos = 0
+        Pos.ypos = 0
+        # print(Pos.xpos, Pos.ypos)
+        add_value(0, 0, "T")
+        print_path()
+        moving_taxi(passenger_posx, passenger_posy)
+
+        print("```````````````````````````````````````````````````````````````````````````````````````````````````````")
+        print("\nYou picked up the passanger. Now drop the passanger off to their home!\n")
+        print("```````````````````````````````````````````````````````````````````````````````````````````````````````")
+
+        home_posx, home_posy = randint(0, 9), randint(0, 4)
+        add_value(home_posx, home_posy, "H")
+        add_value(Pos.xpos, Pos.ypos, "T")
+        print_path()
+        moving_taxi(home_posx, home_posy)
+
+        print("```````````````````````````````````````````````````````````````````````````````````````````````````````")
+        print("You dropped off the passanger safely! Thanks for playing the Taxi Game")
+        print("```````````````````````````````````````````````````````````````````````````````````````````````````````")
+
+    elif main_menu == "i":
+        print("```````````````````````````````````````````````````````````````````````````````````````````````````````")
+        print("\nIn this game you will be driving a taxi. Your goal is to pick up you passangers and drop them off \n"
+              "at their drop off locations. You can move around using the W A S D keys. You also have to stay in   \n"
+              "bounds so be cautious of ur bondary!")
+        print("```````````````````````````````````````````````````````````````````````````````````````````````````````")
+
+    else:
+        game = False
